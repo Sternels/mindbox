2229c589d164ca5cc701cb1c5e54f8feae45b115
Sternels <sternel888@gmail.com>
2024-04-17 09:11:21 +0900

    init

---
+++ src/pages/home/index.tsx
@@ -0,0 +1,15 @@
+import { FC } from "react"
+
+interface HomePageProps {
+    className?: string
+}
+
+const HomePage:FC<HomePageProps> = ({className = ""}) => {
+  return (
+    <>
+
+    </>
+  )
+}
+
+export default HomePage;
